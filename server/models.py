d0bb2cb509611ac94f1bf5d3982f01f7eb0e9c88
daniel <danielmutisya203@gmail.com>
2023-06-29 17:30:31 +0300

    completed assignment

--- server/models.py
+++ server/models.py
@@ -1,7 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 
-db = SQLAlchemy()
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
 
 class Game(db.Model, SerializerMixin):
     __tablename__ = 'games'
@@ -25,7 +30,7 @@ class Review(db.Model, SerializerMixin):
     __tablename__ = 'reviews'
 
     serialize_rules = ('-game.reviews', '-user.reviews',)
-
+    
     id = db.Column(db.Integer, primary_key=True)
     score = db.Column(db.Integer)
     comment = db.Column(db.String)
@@ -45,7 +50,7 @@ class User(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-
+    
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
